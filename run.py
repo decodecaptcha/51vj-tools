518609df3711263b6e311f24ce94509039a2a600
shifeng.lin <shifeng.lin@chukou1.com>
2022-06-06 11:19:22 +0800

    add

--- run.py
+++ run.py
@@ -10,37 +10,40 @@ from business.comment import comment
 from business.comment_prise import comment_prise
 from business.community import community
 from business.reader import reader
-from settings import (BUSINESS_SEC_ID, BUSINESS_TYPE, CHANNEL_ID,
+from settings import (JOB_NUMBER, BUSINESS_SEC_ID, BUSINESS_TYPE, CHANNEL_ID,
                         COMMENT_CONTENT, JOB_DELAY_TIME, JOB_HOUR, JOB_MINUTE,
                         ROLE_PERSON_ID)
 
 
 def job():
-    delay = JOB_DELAY_TIME
-    role_person_id = ROLE_PERSON_ID
-    channel_id = CHANNEL_ID
-    business_id = community(role_person_id, channel_id)
-    logger.debug("business_id: ", business_id)
-    time.sleep(delay)
+    for index in range(JOB_NUMBER):
+        delay = JOB_DELAY_TIME
+        role_person_id = ROLE_PERSON_ID
+        channel_id = CHANNEL_ID
+        business_id = community(role_person_id, channel_id)
+        logger.debug("business_id: ", business_id)
+        time.sleep(delay)
 
-    reader(business_id)
-    time.sleep(delay)
+        reader(business_id)
+        time.sleep(delay)
 
-    comment_content = COMMENT_CONTENT
-    comment_id = comment(business_id, comment_content)
-    logger.debug("comment_id", comment_id)
-    time.sleep(delay)
+        comment_content = COMMENT_CONTENT
+        comment_id = comment(business_id, comment_content)
+        logger.debug("comment_id", comment_id)
+        time.sleep(delay)
 
-    business_type = BUSINESS_TYPE
-    business_sec_id = BUSINESS_SEC_ID
-    business_prise(business_id, business_type, business_sec_id)
-    time.sleep(delay)
+        business_type = BUSINESS_TYPE
+        business_sec_id = BUSINESS_SEC_ID
+        business_prise(business_id, business_type, business_sec_id)
+        time.sleep(delay)
 
-    comment_prise(comment_id)
-    time.sleep(delay)
+        comment_prise(comment_id)
+        time.sleep(delay)
 
-    business_delete(business_id)
-    logger.debug("每日任务, 已完成...")
+        business_delete(business_id)
+        logger.debug(f"已完成任务次数: {index}")
+
+    logger.debug("每日任务, 已完成.")
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,30 +1,37 @@
 # -*- coding: utf-8 -*-
 # settings.py
 
-# 企业微信相关的ID
+# --------------- 登录账号(必填, 根据自己 企业微信 账号填写) --------------- #
+# 企业微信相关的ID 
 CORPID = 'wp58xxxxxxxxxxx_xxx-xxx_x_xxxxEg'
 
-# 登录账号获取 cookie
+# 登录账号获取 cookie (必填项)
 USER_COOKIES = 'rememberme-wp58xxxxxxxxxxx_xxx-xxx_x_xxxxEg=xxx; JSESSIONID=wp58xxxxxxxxxxx_xxx-xxx_x_xxxxEg_xxx'
 
-# :param ROLE_PERSON_ID: 自己的 id, 设置仅自己可见
+# 自己的 id, 设置仅自己可见 (必填项)
 ROLE_PERSON_ID = '24xxxxx1'
 
-# :param CHANNEL_ID: 话题 id, 如 932765 对应话题 "吃瓜群众"
+
+# --------------- 发布动态 (一般不修改) --------------- #
+# 话题 id, 如 932765 对应话题 "吃瓜群众"
 CHANNEL_ID = '932765'
 
 # 评论内容 ≥10字
 COMMENT_CONTENT = '测试测试测试测试测试测试测试测试'
 
 # 目标动态的类型
-# business_type = '40501'
 BUSINESS_TYPE = '40501'
+
 # 目标动态的第 x 条评论 x=0 表示第一条评论
-# business_sec_id = '0'
 BUSINESS_SEC_ID = '0'
 
-# 定时任务
+
+# --------------- 定时任务 (一般不修改) --------------- #
+# 任务之间间隔时间
 JOB_DELAY_TIME = 10
+
+# 任务次数, 默认执行3次, 详情查看《积分规则.html》
+JOB_NUMBER = 3
 
 # 默认是每天的10：30 分执行任务(24小时制, 可自定义)
 JOB_HOUR = 10
